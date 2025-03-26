16172dcf3e6ac428c4139ba71617f10b40e8f439
Vamsi krishna <55651940+kronosvamsi@users.noreply.github.com>
2025-03-26 06:41:30 +0000

    Created a careers page successfully

--- app.py
+++ app.py
@@ -1,11 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+Jobs = [{
+    'id': 1,
+    'title': "Software Engineer",
+    'location': "Banglore, India",
+    'salary': "Rs 10,00,000"
+}, {
+    'id': 2,
+    'title': "Backend Engineer",
+    'location': "Pune, India",
+    'salary': "Rs 15,00,000"
+}, {
+    'id': 3,
+    'title': "Data Scientist",
+    'location': "Hyderabad, India",
+    'salary': "Rs 14,00,000"
+}, {
+    'id': 4,
+    'title': "Frontend Engineer",
+    'location': "Newyork, USA",
+    'salary': "$ 10,000"
+}]
 
 
 @app.route("/")
 def hello_world():
-  return "<h1>Hello, World!<h1p>"
+  return render_template('home.html', jobs=Jobs, company_name="Kronos")
 
 
 if __name__ == "__main__":
